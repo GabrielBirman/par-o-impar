d64d1083acc69cd7260d0a35769450b3e95d7d1c
Gabriel Birman <estebabirman2002@gmail.com>
2025-04-14 18:41:39 -0300

    Final p´1

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 
-let num1=0;
-let num2=0;
+document.getElementById('btn').addEventListener('click', function() {
+
+let numero = parseInt(document.getElementById('numero').value);
+while
+    (isNaN(numero) || numero < 0) {
+        alert("Por favor, ingrese un número válido y mayor o igual a 0.");
+        numero = parseInt(prompt("Ingrese un número válido y mayor o igual a 0:"));
+    }
 
 function esParOImpar(numero) {
+    
     if (numero % 2 == 0) {
         return "par";
     } else {
         return "impar";
     }
 }
+let resultado = esParOImpar(numero);
+document.getElementById('resultado').alert(resultado);
 
+
+});
