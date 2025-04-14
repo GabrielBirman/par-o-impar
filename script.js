8663d67cb78d62e12507c4fbcbae25930665f3b8
Gabriel Birman <estebabirman2002@gmail.com>
2025-04-14 18:59:18 -0300

    fin

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 
 
 document.getElementById('btn').addEventListener('click', function() {
-    document.getElementById('btn').addEventListener('click', function() {
+    
         console.log("Agrega dos numeros y te los multiplica");
-      let num = parseInt(prompt("Escribe el primer numero: "));
-      function esParOImpar(num) {
+    let num;
+    do {
+        num = parseInt(prompt("Escribe un numero VALIDO y mayor o igual a 0: "));
+    } while (isNaN(num) || num < 0);   
+    
+    function esParOImpar(num) {
         if (num % 2 == 0) {
           return "par";
         } else {
@@ -12,10 +16,10 @@ document.getElementById('btn').addEventListener('click', function() {
         }
       }
       
-      let resultado = esParOImpar(num1,num2);
+      let resultado = esParOImpar(num);
       alert("El resultado es: " + resultado);
       });
       
 
 
-});
+
